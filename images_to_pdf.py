caa613ca079a72c21bb664cf8846ef0aeab1a23c
Sai Ashish <saiashishkonchada@gmail.com>
2023-06-28 12:31:27 +0530

    Update images_to_pdf.py

--- images_to_pdf.py
+++ images_to_pdf.py
@@ -4,44 +4,23 @@ from PIL import Image, ImageTk
 from reportlab.lib.pagesizes import landscape
 from reportlab.pdfgen import canvas
 
-
 class ImageToPDFConverter:
     def __init__(self, root):
         self.root = root
         self.image_paths = []
-
         self.root.title("Image to PDF Converter")
         self.root.geometry("750x600")
-
-        self.select_images_button = tk.Button(
-            self.root,
-            text="Select Images",
-            command=self.select_images,
-            font=("Helvetica", 12),
-        )
+        self.select_images_button = tk.Button(self.root, text="Select Images", command=self.select_images, font=("Helvetica", 12),)
         self.select_images_button.pack(pady=10)
-
-        self.convert_to_pdf_button = tk.Button(
-            self.root,
-            text="Convert to PDF",
-            command=self.convert_to_pdf,
-            font=("Helvetica", 12),
-        )
+        self.convert_to_pdf_button = tk.Button(self.root, text="Convert to PDF", command=self.convert_to_pdf,font=("Helvetica", 12),)
         self.convert_to_pdf_button.pack(pady=10)
-
-        self.select_images_label = tk.Label(
-            self.root, text="Select Images", font=("Helvetica", 14)
-        )
+        self.select_images_label = tk.Label(self.root, text="Select Images", font=("Helvetica", 14))
         self.select_images_label.pack(pady=10)
-
         self.preview_frame = tk.Frame(self.root, width=380, height=200)
         self.preview_frame.pack(pady=10)
 
     def select_images(self):
-        self.image_paths = filedialog.askopenfilenames(
-            initialdir="/", title="Select Images", filetypes=(("Image Files", "*.jpg *.png"),)
-        )
-
+        self.image_paths = filedialog.askopenfilenames(initialdir="/", title="Select Images", filetypes=(("Image Files", "*.jpg *.png"),))
         self.preview_frame.destroy()
         self.preview_frame = tk.Frame(self.root, width=380, height=200)
         self.preview_frame.pack(pady=10)
@@ -50,7 +29,6 @@ class ImageToPDFConverter:
             image = Image.open(image_path)
             image = self.resize_image(image, width=150, height=150)
             photo = ImageTk.PhotoImage(image)
-
             label = tk.Label(self.preview_frame, image=photo)
             label.image = photo
             label.grid(row=i // 3, column=i % 3, padx=10, pady=10)
@@ -63,29 +41,16 @@ class ImageToPDFConverter:
         return resized_image
 
     def convert_to_pdf(self):
-        if not self.image_paths:
-            messagebox.showwarning("No Images Selected", "Please select images first!")
-            return
-
-        pdf_path = filedialog.asksaveasfilename(
-            defaultextension=".pdf", filetypes=(("PDF Files", "*.pdf"),)
-        )
-
-        if not pdf_path:
-            return
-
+        pdf_path = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=(("PDF Files", "*.pdf"),))
         c = canvas.Canvas(pdf_path, pagesize=landscape)
-
         for image_path in self.image_paths:
             image = Image.open(image_path)
             width, height = image.size
             c.setPageSize((width, height))
             c.drawImage(image_path, 0, 0, width=width, height=height)
             c.showPage()
-
         c.save()
         messagebox.showinfo("Conversion Successful", f"PDF saved at {pdf_path}")
-
 
 if __name__ == "__main__":
     root = tk.Tk()
